ef719d3e4f50e2935534978c8a755eb7d4cd3db2
Igor Chiriac <igorchiriac@outlook.com>
2021-11-22 18:33:05 +0100

    Add login/logout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,44 @@
-import React, {useContext, useEffect} from 'react';
+import { useContext, useState } from 'react';
 import './App.css';
 import { AuthContext } from './context/AuthContext';
 
-
 function App() {
-  const { logInUser, isLoading, user } = useContext(AuthContext);
-  useEffect(()=>{
-    logInUser()
-  }, [])
-  return (
-    <div className="App">
-      <header className="App-header">
-        {isLoading && 'Loading'}
-        {user && 'Current user is: ' + user?.username}
-      </header>
-    </div>
-  );
+    const { logInUser, isLoading, user, logOutUser } = useContext(AuthContext);
+    const [formData, setFormData] = useState({ username: '', password: '' });
+    return (
+        <div className="App">
+            <header className="App-header">
+                {isLoading && 'Loading'}
+                {user ? (
+                    <div>
+                        <span>Current user is: {user?.username}</span>
+                        <br />
+                        <button onClick={() => logOutUser()}>Log out </button>
+                    </div>
+                ) : (
+                    <div>
+                        <input
+                            type="text"
+                            name="username"
+                            value={formData.username}
+                            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+                        />{' '}
+                        <br />
+                        <input
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                        />
+                        <br />
+                        <button type="button" onClick={() => logInUser(formData)}>
+                            Log in
+                        </button>
+                    </div>
+                )}
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,45 +1,38 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const AuthContext = React.createContext({} as any);
 
 function AuthProviderWrapper(props: any) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const [user, setUser] = useState(null);
-  
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [user, setUser] = useState(null);
 
+    const logInUser = async ({ username, password }: { username: string; password: string }) => {
+        try {
+            await axios.post('/api/v1/token/', {
+                username: username,
+                password: password,
+            });
+            let response = await axios.get('/api/v1/users/me/');
+            setUser(response.data);
+            setIsLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  const logInUser = async (token: any) => {
-    try {
-        await axios.post('/api/v1/token/', {
-            username: 'Curro',
-            password: '1223334444'
-        })
-        let response = await axios.get('/api/v1/users/me/') 
-        setUser(response.data)
-        setIsLoading(false)
+    const logOutUser = () => {
+        sessionStorage.removeItem('authToken');
+        setIsLoggedIn(false);
+        setUser(null);
+    };
 
-    } catch (error) {
-        console.log(error);
-    }
-
-  }
-
-  const logOutUser = () => {
-    sessionStorage.removeItem("authToken");
-    setIsLoggedIn(false);
-    setUser(null);
-  }    
-
-
-  return (
-    <AuthContext.Provider
-      value={{ isLoggedIn, isLoading, user, logInUser, logOutUser }}
-    >
-      {props.children}
-    </AuthContext.Provider>
-  )
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, isLoading, user, logInUser, logOutUser }}>
+            {props.children}
+        </AuthContext.Provider>
+    );
 }
 
 export { AuthProviderWrapper, AuthContext };
