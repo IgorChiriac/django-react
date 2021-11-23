6b0671788b2a85a67b1585e0772242afa02d0e3c
Curro Jimenez <curro.jimenez@grapheverywhere.com>
2021-11-23 19:40:32 +0100

    Register page, first approach

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,43 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import NotFound from './pages/NotFound';
-import Home from './pages/Home';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Login from "./pages/Login";
+import SignUp from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <header className="App-header">
-                    <div>
-                        <ul>
-                            <li>
-                                <Link to={'/'}>Home</Link>
-                            </li>
-                            <li>
-                                <Link to={'/login'}>Login</Link>
-                            </li>
-                            <li>
-                                <Link to={'/signup'}>Signup</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </header>
-                <Switch>
-                    <Route exact path="/" component={Home} />
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <ul>
+              <li>
+                <Link to={"/"}>Home</Link>
+              </li>
+              <li>
+                <Link to={"/login"}>Login</Link>
+              </li>
+              <li>
+                <Link to={"/signup"}>Signup</Link>
+              </li>
+            </ul>
+          </div>
+        </header>
+        <Switch>
+          <Route exact path="/" component={Home} />
 
-                    <Route path="/login">
-                        <Login />
-                    </Route>
-                    <Route path="/signup">
-                        <SignUp />
-                    </Route>
-                    <Route component={NotFound} />
-                </Switch>
-            </div>
-        </Router>
-    );
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,124 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { AuthContext } from "../context/AuthContext";
+import UserService from "../services/user";
+
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {"Igor Chiriac "}
+      <Link color="inherit" href="https://www.linkedin.com/in/igor-chiriac/">
+        LinkedIn
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignIn() {
+  const { logInUser, isLoading, user, logOutUser } = React.useContext(AuthContext);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    UserService.createUser({
+      username: String(data.get("username")),
+      password: String(data.get("password")),
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="User name"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="first_name"
+              label="First Name"
+              name="first_name"
+              autoComplete="first_name"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="last_name"
+              label="Last Name"
+              name="last_name"
+              autoComplete="last_name"
+            />
+            <TextField margin="normal" required fullWidth id="email" label="Email" name="Email" autoComplete="email" />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
+            <Button type="submit" disabled={isLoading} fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Sign Up!
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  {"Have an account? Sign In"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
