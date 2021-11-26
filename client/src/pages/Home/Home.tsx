da26ee960d0acf80e964a6c2c149f51821f6e42c
Curro Jimenez <curro.jimenez@grapheverywhere.com>
2021-11-26 18:26:45 +0100

    Home page styling

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -6,6 +6,9 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import RestaurantCard from "../../components/RestaurantCard";
 import ApplicationBar from "../../components/ApplicationBar";
+import Box from "@mui/system/Box";
+import mainImage from "./restaurantreview.jpeg";
+import Typography from "@mui/material/Typography";
 
 const Home = () => {
   const [restaurants, setRestaurants] = useState([]);
@@ -26,17 +29,20 @@ const Home = () => {
           sx={{ backgroundColor: "white", padding: "20px" }}
         >
           <CssBaseline />
-          <Grid
-            container
-            spacing={2}
-            xs={12}
-            alignItems="center"
-            justifyContent="center"
-          >
+          <Box sx={{ border: "2px solid #3e3750", borderRadius: "13px", backgroundColor: "#e9e6e6" }}>
+            <img src={mainImage} style={{ objectFit: "cover", borderRadius: "13px" }} width="100%" height="200px" />
+            <Typography gutterBottom variant="h5" component="div">
+              Rate the taste
+            </Typography>
+            <Typography gutterBottom variant="overline" component="div">
+              Find the best restaurants around! Comments and reviews for everyone
+            </Typography>
+          </Box>
+          <Grid container xs={12} spacing={2} marginTop={2} display="flex" alignContent="center" justifyItems="center">
             {restaurants &&
               restaurants.length &&
               restaurants.map((restaurant) => (
-                <Grid item xs={12}>
+                <Grid item xs={4}>
                   <RestaurantCard restaurant={restaurant} />
                 </Grid>
               ))}
